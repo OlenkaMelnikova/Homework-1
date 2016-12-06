fa623359221d379ccbc9b343cf01f15f328877d6
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-12-06 20:49:00 +0300

    Add files via upload

---
+++ home.cpp
@@ -0,0 +1,70 @@
+// home.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+int  Value(int b, int Gamma)
+{
+	return(b^Gamma);
+}
+int cyclic_shift(int b_Gamma)
+{
+	return ((b_Gamma << 4) | (b_Gamma >> 28));
+}
+int reverse_cyclic_shift(int b_result)
+{
+	return ((b_result >> 4) | (b_result << 28));
+}
+int reverse_Value(int b_pre_Gamma, int Gamma)
+{
+	return(b_pre_Gamma^Gamma);
+}
+int main()
+{
+	char lenght[256];
+	int key;
+	cout << "Input: " << endl;
+	cin.getline(lenght, 256);
+	cout << "Input key: " << endl;
+	cin >> key;
+	cin.clear();
+	srand(key);
+	int vrb = rand();
+	int Gamma = vrb % 256;
+	int n = 1;
+	for (int i = 0; lenght[i] != '\0'; ++i)
+		n++;
+	char *Phrase = (char *)malloc(n * sizeof(char));
+	for (int i = 0; i < n; ++i)
+		Phrase[i] = lenght[i];
+	int *variable = (int *)malloc(n * sizeof(int));
+	cout << endl << "Value: ";
+	for (int i = 0; i < n; ++i) {
+		variable[i] = Value(Phrase[i], Gamma);
+		putchar(variable[i]);
+	}
+	cout << endl << "Cyclic_shift: ";
+	for (int i = 0; i < n; ++i) {
+		variable[i] = cyclic_shift(variable[i]);
+		putchar(variable[i]);
+	}
+	cout << endl << "Reverse_cyclic_shift: ";
+	for (int i = 0; i < n; ++i) {
+		variable[i] = reverse_cyclic_shift(variable[i]);
+		putchar(variable[i]);
+	}
+	cout << endl << "Reverse_Value: ";
+	for (int i = 0; i < n; ++i){
+		variable[i] = reverse_Value(variable[i], Gamma);
+		putchar(variable[i]);
+	}
+	cout << "\n";
+	system("pause");
+	return 0;
+}
+
+
